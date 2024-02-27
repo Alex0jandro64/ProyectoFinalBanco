0a23bbd233ce79e1464aa0172a9c65ddbabe63b8
Alex0jandro64 <alejandroalcerreca3@gmail.com>
2024-02-27 19:02:08 +0100

    v1.4

--- src/app/componentes/transaccion/transaccion-routing.module.ts
+++ src/app/componentes/transaccion/transaccion-routing.module.ts
@@ -4,11 +4,11 @@ import { TransaccionComponent } from './transaccion.component';
 import { AutenticacionComponent } from '../autenticacion/autenticacion.component';
 import { NuevaTransaccionComponent } from './nueva-transaccion/nueva-transaccion.component';
 import { ListaTransaccionesComponent } from './lista-transacciones/lista-transacciones.component';
-
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 const routes: Routes = [
   { path: '', component: AutenticacionComponent, children:[
-    { path: 'nueva-transaccion', component: NuevaTransaccionComponent},
-    { path: 'lista-transacciones', component: ListaTransaccionesComponent},
+    { path: 'nueva-transaccion', component: NuevaTransaccionComponent, ...canActivate(()=> redirectUnauthorizedTo(['/autenticacion/login']))},
+    { path: 'lista-transacciones', component: ListaTransaccionesComponent, ...canActivate(()=> redirectUnauthorizedTo(['/autenticacion/login']))},
   ]}
 ];
 
